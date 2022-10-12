bea38c1946af04ee19c54dcb3c7b0db692a125d1
Sanketdeveloper24 <sanketlanjewar11@gmail.com>
2022-10-12 17:15:37 +0530

    code

---
+++ mi.js
@@ -0,0 +1,9 @@
+function loadCoupon() {
+  document.getElementById("coupon").style.visibility = "visible";
+  document.getElementById("sad").style.opacity = "0.7";
+}
+
+function closeCoupon() {
+  document.getElementById("coupon").style.visibility = "hidden";
+  document.getElementById("sad").style.opacity = "1";
+}
