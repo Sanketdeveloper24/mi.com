4a2d39ef322f5c78c0060d627985811ff38d48e3
Sanketdeveloper24 <sanketlanjewar11@gmail.com>
2022-10-16 11:44:24 +0530

    code

--- mi.js
+++ mi.js
@@ -7,3 +7,13 @@ function closeCoupon() {
   document.getElementById("coupon").style.visibility = "hidden";
   document.getElementById("sad").style.opacity = "1";
 }
+
+function changeMode() {
+  let mybody = document.body;
+  mybody.classList.toggle("mydark");
+  if (mybody.classList.contains("mydark")) {
+    icon.src = "images/sun.png";
+  } else {
+    icon.src = "images/moon.png";
+  }
+}
